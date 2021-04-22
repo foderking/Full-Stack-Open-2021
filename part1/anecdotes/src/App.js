bf381a1777bfca09e8bfa43eb1e28820190852fd
fenix <freatshow@gmail.com>
2021-04-23 00:46:03 +0100

    completed 1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -10,12 +10,24 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0])
+
   const next = () =>
     setSelected(Math.floor(Math.random() * anecdotes.length))
 
+  const increaseVotes = () => {
+    let newVotes = {...votes}
+    newVotes[selected] += 1
+    setVotes(newVotes)
+  }
+
   return(
     <div>
-      <p>{anecdotes[selected]}</p>
+      <p>{anecdotes[selected]}.</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={() => increaseVotes()}>
+        vote
+      </button>
       <button onClick={next}>
         next anecdote
       </button>
