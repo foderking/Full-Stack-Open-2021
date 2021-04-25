04fc0df4413f67751384bc2806684c6fd2dbbd13
fenix <freatshow@gmail.com>
2021-04-25 12:45:59 +0100

    completed exercise 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -10,6 +10,7 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [no, setNo] = useState('')
   const [filter, setFilter] = useState('')
+  const stateData = [['Name', newName, setNewName], ['Number', no, setNo]]
 
   const search = (value) => {
     return value.name.toLowerCase().startsWith(filter.toLowerCase())
@@ -17,39 +18,71 @@ const App = () => {
   const submitNew = (event) => {
     event.preventDefault()
     const newPers = {name: newName,
-                    number: no}
-
+                     number: no }
     if (persons.filter(each => each.name === newName).length) {
         window.alert(`${newName} is already added to Phonebook`)
     } else {
         setPersons(persons.concat(newPers))
-      }
+    }
   }
-  
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>filter shown with <input value={filter} onChange={(event) => setFilter(event.target.value)} /></div>
-      <h2>add a new</h2>
-      <form onSubmit={submitNew}>
-        <div>
-          name: <input value={newName} onChange={(event) => setNewName(event.target.value)}/>
-          <br />
-          number: <input value={no} onChange={(event) => setNo(event.target.value)}/>
-        </div>
-        <div>
-          <button type="submit" >add</button>
-        </div>
-      </form>
+      <Filter value={filter} search={setFilter} />
+      <h2>Add a new</h2>    
+      <AddPerson data={stateData} submit={submitNew} />
       <h2>Numbers</h2>
-        {persons.filter(
-            each => search(each)
-          ).map(
-          each => <p key={each.name}>{each.name} {each.number}</p>
-          )}
+      <ShowPersons data={persons} search={search} />
     </div>
   )
 }
+
+const Filter = (props) => 
+  <div>
+    Filter shown with <input 
+      value={props.value} 
+      onChange={
+        (event) => props.search(event.target.value)
+      }
+  />
+  </div>  
+
+
+const AddPerson = ({data, submit}) => 
+  <div>
+    <form onSubmit={submit}>
+      {data.map(
+        each => <Input key={each[0]} text={each[0]} value={each[1]} handler={each[2]} />
+      )}
+      <button type="submit" >add</button>
+    </form>  
+  </div>
+
+const Input = (props) => 
+  <div>
+    {props.text}: <input 
+      value={props.value} 
+      onChange={
+        (event) => props.handler(event.target.value)
+      } 
+    />
+  </div>
+
+
+const ShowPersons = ({data, search}) => {
+  const filteredList = data.filter(each => search(each))
+  return (
+    <div>
+    {
+      filteredList.map(
+        each => <EachPerson key={each.name} name={each.name} number={each.number} />
+      )
+    }
+    </div>    
+  )
+}
+
+const EachPerson = ({name, number}) => <p>{name} {number}</p>
 
 
 export default App
