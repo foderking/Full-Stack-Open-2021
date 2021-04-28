771990996a2ef87a92f88da3fda2a4c4ec72d270
fenix <freatshow@gmail.com>
2021-04-28 22:26:17 +0100

    completed exercise 2.13

--- part2/data for countries/src/App.js
+++ part2/data for countries/src/App.js
@@ -5,8 +5,9 @@ import axios from 'axios'
 const App = () => {
   const [state, setState] = useState([])
   const [query, setQuery] = useState('')
+  const [country, setCountry] = useState({name: ''})
 
-  // finds names in phonebook that start with name stored in the"filter" state
+  // finds names in phonebook that start with name stored in the "filter" state
   const search = (value) => {
     return value.name.toLowerCase().startsWith(query.toLowerCase())
   }
@@ -22,25 +23,34 @@ const App = () => {
 
   return (
     <div>
-      <Filter search={setQuery} data={query}/>
-      <ViewCountries search={search} data={state} />
+      <Filter 
+        search={setQuery} 
+        data={query} 
+        change={setCountry} 
+      />
+      <ViewCountries 
+        search={search} 
+        data={state} 
+        changeCountry={setCountry} 
+        country={country}/>
     </div>
   )
 }
 
-const ViewCountries = ({data, search}) => {
+const ViewCountries = ({data, search, changeCountry, country}) => {
   // filters data to only names that match input
   const filteredList = data.filter(
     each => search(each)
   )
-  if (filteredList.length > 10) {
+
+  if (filteredList.length > 10) {     // handles when the search results are > 10
     return (
       <div>
         Too many matches, specify another filter
       </div>
       )
-  } else if (filteredList.length === 1) {
-    const langList = filteredList[0].languages.map(    // neatly creates an array from the language object
+  } else if (filteredList.length === 1  ) {         // handles when there is only one search result
+    const langList = filteredList[0].languages.map(     // neatly creates an array from the language object
       each => each.name
     )
     return (
@@ -52,11 +62,31 @@ const ViewCountries = ({data, search}) => {
         flag={filteredList[0].flag}
       />
       )
-  } else {
+  } else if (country.name !== ''){        // handles when the "show" button is clicked
+    return (
+      <DisplayCountry 
+        name={country.name} 
+        capital={country.capital}
+        population={country.population}
+        laguages={country.languages.map(
+          each => each.name
+        )}
+        flag={country.flag}
+      />
+      )
+  } else {                              // handles when there are between 1 - 10 search results
     return (
       <div>
         {filteredList.map(
-          each => <p key={each.name} >{each.name}</p>
+          each => 
+            <div key={each.name}>
+              {each.name}
+              <button onClick={
+                (event) => { changeCountry(each) }
+              }>
+                show
+              </button>
+            </div>
           )}
       </div>
       )
@@ -68,7 +98,10 @@ const Filter = (props) =>
     <div>
       Find countries 
       <input type='text' value={props.data} onChange={
-        (event) => props.search(event.target.value)
+        (event) => {
+          props.search(event.target.value)
+          props.change({name: ''})
+        }
       }/>
     </div>
 
@@ -94,5 +127,6 @@ const DisplayCountry = (props) => {
     </div>
     )
 }
+
 
 export default App
