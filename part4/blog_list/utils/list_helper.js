9cc6cc02f4291f68d86dbdc773b230fe38221753
foderking <freatshow@gmail.com>
2021-05-08 15:41:15 -0700

    completed 4.5

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -3,14 +3,24 @@ const dummy = blogs => {
 }
 
 const totalLikes = blogs => {
-	total = 
+	const total = 
 		blogs
 			.map(each => each.likes)
 			.reduce((sum, item) => sum + item, 0)
 	return total
 }
 
+const favoriteBlog = blogs => {
+	const maxlike = 
+		blogs
+			.map(each => each.likes)
+			.reduce((acc, other) => Math.max(acc, other), 0)
+	const blog = blogs.find(blogs => blogs.likes === maxlike)
+	return blog
+}
+
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }
