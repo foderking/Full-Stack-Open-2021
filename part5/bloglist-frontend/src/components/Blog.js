11c629e3e03a39ba26ea9e203e141ed4c95ed982
foderking <freatshow@gmail.com>
2021-05-30 10:28:55 +0100

    completed 5.6 & 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+const Blog = ({blog}) => {
+  const [seeFull, setSee] = useState(true)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {seeFull ?
+        <PartBlog blog={blog} /> :
+        <FullBlog blog={blog} />
+      }
+      <button onClick={() => setSee(!seeFull)}>
+        {seeFull ? 'see more' : 'see less'} 
+      </button>
+    </div>  
+  )
+}
+
+const PartBlog = ({blog}) => 
+  <>
+    {blog.title}  {blog.author}
+  </>
+
+const FullBlog = ({blog}) =>
+  <>
+    <div>{blog.title}</div>
+    <div>{blog.author}</div>
+    <div>{blog.url}</div>
+    <div>
+      likes {blog.likes}
+      <button>like</button>
+    </div>
+  </>
 
 export default Blog
