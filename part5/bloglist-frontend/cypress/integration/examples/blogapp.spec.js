6c458e290fa114f250c3dc5bc5d04c042aee74ab
foderking <freatshow@gmail.com>
2021-06-06 21:21:18 +0100

    completed 5.19

--- part5/bloglist-frontend/cypress/integration/examples/blogapp.spec.js
+++ part5/bloglist-frontend/cypress/integration/examples/blogapp.spec.js
@@ -26,4 +26,25 @@ describe('Blog app', function() {
 			cy.get('.error').contains('invalid username or password')
 		})
 	})
+
+	describe('when logged in', function (){
+		const blog = {title: 'random title', author: 'random author', url: 'random url' }
+		
+		beforeEach( function() {
+			cy.get('#Username').type('foking')
+			cy.get('#Password').type('random')
+			cy.contains('login').click()
+			cy.contains('foking logged in successfully')	
+		})
+
+		it('a blog can be created', function (){
+			cy.get('#Title').type(blog.title)
+			cy.get('#Author').type(blog.author)
+			cy.get('#Url').type(blog.url)
+
+			cy.contains('create').click()
+			cy.contains('random title posted successfully')
+			cy.contains('random title random author')
+		})
+	})
 })
