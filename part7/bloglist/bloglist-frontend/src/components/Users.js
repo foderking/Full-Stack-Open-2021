a7ba58698517f17d6db86381b90e3bd4752b46f5
foderking <freatshow@gmail.com>
2021-07-22 06:07:26 -0700

    completed 7.14

--- part7/bloglist/bloglist-frontend/src/components/Users.js
+++ part7/bloglist/bloglist-frontend/src/components/Users.js
@@ -4,11 +4,19 @@ import blogService from '../services/blogs'
 import { useSelector, useDispatch } from 'react-redux'
 import { helper } from '../reducers/Reducer'
 import LoggedIn from './LoggedIn'
+import { Route, useParams, useRouteMatch } from 'react-router'
+import { Link } from 'react-router-dom'
 
 const Users = ({ notify }) =>
 {
+	const { path , url } = useRouteMatch()
+
 	const dispatch = useDispatch()
 	const allUser = useSelector(state => state.allUser)
+	const currentUser = useSelector(state => state.user).username
+
+	console.log(allUser)
+
 
 
 	useEffect(() => {
@@ -24,13 +32,47 @@ const Users = ({ notify }) =>
 		<div>
 			<h2>blogs</h2>
 			<LoggedIn notify={notify} />
-			<ShowInfo state={allUser} />
+			<Route path={`${path}/:id`} >
+				<EachUser current={currentUser} allUser={allUser} />
+			</Route>
+			<Route exact path={`${path}`} >
+				<ShowInfo state={allUser} url={url} />
+			</Route>
+		</div>
+	)
+}
+
+const EachUser = ({ current, allUser }) =>
+{
+	const  routeId  = useParams().id
+
+	const rnad = allUser.find(each => each.id === routeId  && each.username === current)
+	console.log(rnad, routeId)
+
+	if (!rnad) {
+		return(
+			<h3>Invalid User</h3>
+		)
+	}
+
+	return (
+		<div>
+			<h2>{current}</h2>
+			<h3>added blogs</h3>
+
+			<ul>
+				{
+					rnad.blogs.map( each =>
+						<li key={each.id} >{each.title}</li>
+					)
+				}
+			</ul>
 		</div>
 	)
 }
 
 
-const ShowInfo =({ state }) =>
+const ShowInfo =({ state, url }) =>
 {
 	return (
 		<div>
@@ -45,7 +87,11 @@ const ShowInfo =({ state }) =>
 					{
 						state.map(each =>
 							<tr key={each.id} >
-								<td>{each.username}</td>
+								<td>
+									<Link to={`${url}/${each.id}`} >
+										{each.username}
+									</Link>
+								</td>
 								<td>{each.blogs.length}</td>
 							</tr>
 						)
