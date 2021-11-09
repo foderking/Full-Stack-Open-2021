49fbe017ef8731d7f43575e5cdc59ea757087f17
foderking <freatshow@gmail.com>
2021-11-09 22:28:16 +0100

    done 9.18

--- part9/pat_front/src/state/reducer.ts
+++ part9/pat_front/src/state/reducer.ts
@@ -1,10 +1,14 @@
 import { State } from "./state";
-import { Patient } from "../types";
+import { Patient, Diagnosis } from "../types";
 
 export type Action =
   | {
       type: "SET_PATIENT_LIST";
       payload: Patient[];
+    }
+	| {
+      type: 'SET_DIAGNOSES_LIST';
+      payload: Diagnosis[];
     }
   | {
       type: "ADD_PATIENT";
@@ -32,7 +36,33 @@ export const reducer = (state: State, action: Action): State => {
           [action.payload.id]: action.payload
         }
       };
+    case 'SET_DIAGNOSES_LIST':
+      return {
+        ...state,
+        diagnoses: {
+          ...action.payload.reduce(
+            (memo, diagnosis) => ({ ...memo, [diagnosis.code]: diagnosis }),
+            {}
+          ),
+          ...state.diagnoses
+        }
+      };
     default:
       return state;
   }
+};
+
+
+export const setPatientList = (patientList: Patient[]): Action => {
+	return {
+		type: 'SET_PATIENT_LIST',
+		payload: patientList
+	};
+};
+
+export const setDiagnosesList = (diagnosesList: Diagnosis[]): Action => {
+	return {
+		type: 'SET_DIAGNOSES_LIST',
+		payload: diagnosesList
+	};
 };
